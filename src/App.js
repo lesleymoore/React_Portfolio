57ed90e687c245b647487a49e6acab664b8a2e15
Lesley Moore <ldmoore15@gmail.com>
2021-08-26 17:23:18 -0400

    removed apollo?

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import About from './aboutme.js';
 import Portfolio from './portfolio.js'
@@ -7,14 +6,10 @@ import Contact from './contact.js'
 import Resume from './resume.js'
 import './App.css';
 
-const client = new ApolloClient({
-  uri: '/graphql',
-  cache: new InMemoryCache(),
-});
 
 function App() {
   return (
-    <ApolloProvider client= {client}>
+
     <Router>
       <div classname="App"></div>
       <Route exact path="/React_Portfolio" component={About} />
@@ -22,7 +17,7 @@ function App() {
       <Route path="/Contact" component={Contact} />
       <Route path="/Resume" component={Resume} />
     </Router>
-    </ApolloProvider>
+    
   );
 }
 
